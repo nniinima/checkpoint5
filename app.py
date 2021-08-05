ebe78f8f01df9adfbf78be81147d2df48556549b
nniinima <niko.niinimaki@gmail.com>
2021-08-05 16:24:16 +0300

    switched to postgresql

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sqlite3
+import psycopg2
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 from datetime import datetime
@@ -6,22 +6,23 @@ from init_db import do_init
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    conn = psycopg2.connect(
+    host="localhost",
+    database="blogdb",
+    user="postgres")
     return conn
-
-#
-
 
 def get_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM posts WHERE id = %s',
+                        (post_id,))
+    columns = list(cur.description)
+    post = cur.fetchall()
     conn.close()
     if post is None:
         abort(404)
-    return post
-
+    return (post, columns)
 
 app = Flask(__name__)
 do_init()
@@ -40,23 +41,32 @@ def format_date(post_date):
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM posts')
+    columns = list(cur.description)
+    result = cur.fetchall()
     conn.close()
-    # we need to iterate over all posts and format their date accordingly
-    dictrows = [dict(row) for row in posts]
-    for post in dictrows:
-        # using our custom format_date(...)
-        post['created'] = format_date(post['created'])
-    return render_template('index.html', posts=dictrows)
+    # we need to iterate over all posts and format their date accordingly.
+    posts = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        posts.append(row_dict)
+    return render_template('index.html', posts=posts)
 
 
 # here we get a single post and return it to the browser
 @app.route('/<int:post_id>')
 def post(post_id):
-    post = dict(get_post(post_id))
-    post['created'] = format_date(post['created'])
-    return render_template('post.html', post=post)
-
+    result, columns = get_post(post_id)
+    post = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        post.append(row_dict)
+    return render_template('post.html', post=post[0])
 
 # here we create a new post
 @app.route('/create', methods=('GET', 'POST'))
@@ -69,7 +79,8 @@ def create():
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+            cur = conn.cursor()
+            cur.execute('INSERT INTO posts (title, content) VALUES (%s, %s)',
                          (title, content))
             conn.commit()
             conn.close()
@@ -80,7 +91,13 @@ def create():
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
-    post = get_post(id)
+    result, columns = get_post(id)
+    post = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        post.append(row_dict)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -90,23 +107,31 @@ def edit(id):
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
+            cur = conn.cursor()
+            cur.execute('UPDATE posts SET title = %s, content = %s'
+                         ' WHERE id = %s',
                          (title, content, id))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
 
-    return render_template('edit.html', post=post)
+    return render_template('edit.html', post=post[0])
 
 
 # Here we delete a SINGLE post.
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
-    post = get_post(id)
+    result, columns = get_post(id)
+    post = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        post.append(row_dict)
     conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    cur = conn.cursor()
+    cur.execute('DELETE FROM posts WHERE id = %s', (id,))
     conn.commit()
     conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
+    flash('"{}" was successfully deleted!'.format(post[0]['title']))
     return redirect(url_for('index'))
